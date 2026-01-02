bd95b02588c93c6a0cd66f1ca461f4e430e10569
Viagounet <ismael.rousseau@gmail.com>
2026-01-02 19:03:57 +0100

    add: reward graph & changed nn architecture

--- environment.py
+++ environment.py
@@ -3,6 +3,7 @@ import shimmy
 import numpy as np
 import gymnasium as gym
 import imageio
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from copy import deepcopy
@@ -40,7 +41,7 @@ def generate_value_func_training_examples(rewards: list[float], states: list[np.
 states = []
 rewards = []
 
-value_function_params = initialize_linear_nn_params((168*128,30,30,30,6))
+value_function_params = initialize_linear_nn_params((84*64,300,6))
 
 HYPERPARAMS = {"initial_lr": 1e-3, "final_lr": 0, "lr_regime": "linear"}
 
@@ -62,7 +63,7 @@ def simulate_from_state_best_policy(env, previous_frames, value_function_params)
     for action_type in range(env.action_space.n):
         observation, reward, terminated, truncated, info = env.step(action_type)
         previous_frames.append(observation)
-        state = frames_to_bw_resized(previous_frames, 4, (168,128)) 
+        state = frames_to_bw_resized(previous_frames, 4, (84,64)) 
         value, activations = infer(state.reshape(-1).reshape(1,-1) / 255, value_function_params)
         values.append(value.item())
         env.unwrapped.restore_state(original_state_snapshot)
@@ -74,9 +75,16 @@ N_GAMES = 1
 action_choice_strategy = "random"
 print(f"STARTING GAME NUMBER 1")
 actions = []
+actions_for_train = []
+states_for_train = []
+rewards_for_train = []
 frames.append(observation)
-state = frames_to_bw_resized(frames, 4, (168,128)) 
-while N_GAMES < 100:
+state = frames_to_bw_resized(frames, 4, (84,64))
+TRAIN_EVERY = 8
+best_reward = -100
+sum_rewards_history = []
+n_games_indices = []
+while best_reward < 20:
     action_choice_strategy = "best value"
     # predicted_values = simulate_from_state_best_policy(env, frames, value_function_params)
     # action = env.action_space.sample()
@@ -87,18 +95,45 @@ while N_GAMES < 100:
     actions.append(action)
     observation, reward, terminated, truncated, info = env.step(action)
     frames.append(observation)
-    state = frames_to_bw_resized(frames, 4, (168,128)) 
+    state = frames_to_bw_resized(frames, 4, (84,64)) 
     # 
     # save_bw(state, path=Path(f"exports/{_}.png"))
     states.append(state)
     rewards.append(float(reward))
     if terminated or truncated:
         print(f"GAME {N_GAMES} ENDED: REWARD => {sum(rewards)}")
-        gif_path = Path(f"exports/pong/game_{N_GAMES}.gif")
+        sum_rewards_history.append(sum(rewards))
+        n_games_indices.append(N_GAMES)
+        if sum(rewards) > best_reward:
+            best_reward = sum(rewards)
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(n_games_indices, sum_rewards_history, marker='o', linestyle='-', color='b')
+        plt.title('Total reward vs Number of games')
+        plt.xlabel('Game number')
+        plt.ylabel('Total reward')
+        plt.grid(True)
+        plot_path = Path(f"exports/pong/graphs/rewards.png")
+        plt.savefig(plot_path)
+        plt.close()
+
+        gif_path = Path(f"exports/pong/games/game_{N_GAMES}.gif")
         imageio.mimsave(gif_path, frames, fps=30)
         N_GAMES += 1
-        x_train, y_train = generate_value_func_training_examples(rewards, states, actions, 0.95) # x_train = reshaped states, y_train = estimated values
-        value_function_params = train(x_train, y_train, value_function_params, HYPERPARAMS, 250)
+
+        
+
+        rewards_for_train += rewards
+        states_for_train += states
+        actions_for_train += actions
+
+        if N_GAMES % 8 == 0:
+            x_train, y_train = generate_value_func_training_examples(rewards_for_train, states_for_train, actions_for_train, 0.95) # x_train = reshaped states, y_train = estimated values
+            actions_for_train = []
+            states_for_train = []
+            rewards_for_train = []
+            value_function_params = train(x_train, y_train, value_function_params, HYPERPARAMS, 250)
+
         observation, info = env.reset()
         frames = []
         states = []
